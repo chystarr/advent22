d9f88459fbf6cf53e6bb077f31a5a11c214ba149
chystarr <chynnastarr2@gmail.com>
2022-12-04 12:19:33 -0500

    Changed variable name to make more sense for part 2

--- day4/day4.py
+++ day4/day4.py
@@ -3,7 +3,7 @@
 def main():
     file = open("input.txt")
 
-    fully_contained = 0
+    contained = 0
     for line in file:
         assignments = line.strip().split(",")
         first, second = assignments[0].split("-"), assignments[1].split("-")
@@ -20,13 +20,13 @@ def main():
         # Part 2
         # check if the first range partially or fully includes the second
         if (first[0] <= second[0] and first[1] >= second[0]) or (first[0] <= second[1] and first[1] >= second[1]):
-            fully_contained += 1
+            contained += 1
         # check if the second range partially or fully includes the first
         elif (second[0] <= first[0] and second[1] >= first[0]) or (second[0] <= first[1] and second[1] >= first[1]):
-            fully_contained += 1
+            contained += 1
 
     file.close()
-    print(fully_contained)
+    print(contained)
 
 if __name__ == "__main__":
     main()
